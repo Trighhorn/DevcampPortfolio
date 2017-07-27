33b7d7fe5252283419ca384899bba07ffd1b9533
Jordan Hudgens <jordan@devcamp.com>
2017-07-27 16:36:24 +0000

    implemented actioncable for comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show] do
-  put :sort, on: :collection
+    put :sort, on: :collection
   end
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   get 'tech-news', to: 'pages#tech_news'
-  
   
   resources :blogs do
     member do
